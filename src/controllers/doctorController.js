40e203acf96fc8fa8b1900186585678350231208
ngochoanit <phamngochoan.it@gmail.com>
2021-09-18 09:17:35 +0700

    create table markdown and add reltionship table user versus markdown and create api add detail doctor

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -15,6 +15,49 @@ const handleGetTopDoctorHome = async (req, res) => {
         })
     }
 }
+// handle get all doctor
+const handleGetAllDoctors = async (req, res) => {
+    try {
+        const response = await doctorService.getAllDoctorsService()
+        return res.status(200).json(response)
+
+    } catch (err) {
+        console.log(err)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+// handle post infor doctor
+const handlePostInforDoctor = async (req, res) => {
+    try {
+        const response = await doctorService.postInforDoctorSevice(req.body)
+        return res.status(200).json(response)
+
+    } catch (err) {
+        console.log(err)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+const handlegetDetailDoctorById = async (req, res) => {
+    try {
+        const response = await doctorService.getDetailDoctorByIdService(req.query.id)
+        return res.status(200).json(response)
+    } catch (err) {
+        console.log(err)
+        res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
 export const doctorController = {
     handleGetTopDoctorHome,
+    handleGetAllDoctors,
+    handlePostInforDoctor,
+    handlegetDetailDoctorById
 }
